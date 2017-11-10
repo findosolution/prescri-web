c89ea8890e4b84d6d3c80dd1f1e83d33c1ea0f2f
danukaw <danuka.wijetunge@ebuilder.com>
2017-11-10 18:11:45 +0530

    added validation helper class

--- app/components/ValidationHelper.jsx
+++ app/components/ValidationHelper.jsx
@@ -1,3 +1,4 @@
+//patterns
 export var number = new RegExp(/^[-+]?\d*(?:[\.\,]\d+)?$/);
 export var email = new RegExp(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/);
 
